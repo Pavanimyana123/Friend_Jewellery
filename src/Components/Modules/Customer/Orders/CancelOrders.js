0d255f549f2db33c9f32ebacfaa2362b745cf793
bharathkusuma123 <bharathkusuma4953@gmail.com>
2025-03-13 10:46:27 +0530

    bharath

--- src/Components/Modules/Customer/Orders/CancelOrders.js
+++ src/Components/Modules/Customer/Orders/CancelOrders.js
@@ -8,7 +8,7 @@ import './ViewOrders.css';
 import baseURL from '../../../../Url/NodeBaseURL';
 import CustomerNavbar from '../../../Pages/Navbar/CustomerNavbar';
 
-const ViewOrders = () => {
+const CancelOrders = () => {
   const navigate = useNavigate();
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -108,4 +108,4 @@ const ViewOrders = () => {
   );
 };
 
-export default ViewOrders;
+export default CancelOrders;
