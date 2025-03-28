c27b9305f909333680defcb0e966a099e3718d33
pavanimyana123 <pavanimyana2000@gmail.com>
2025-03-28 12:16:18 +0530

    Pavani

--- src/Url/NodeBaseURL.js
+++ src/Url/NodeBaseURL.js
@@ -1,9 +1,9 @@
 
-const baseURL = "http://localhost:5001"; 
+// const baseURL = "http://localhost:5001"; 
 
 // const baseURL = "http://localhost:5000"; 
 
-// const baseURL = "http://213.210.21.78:5000/";
+const baseURL = "http://213.210.21.78:5000/";
 
 
 export default baseURL;
